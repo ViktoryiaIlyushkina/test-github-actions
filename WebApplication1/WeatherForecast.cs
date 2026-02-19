54bc5c82956ada364507bbb1385d470dbc52c364
Viktoryia <151428871+ViktoryiaIlyushkina@users.noreply.github.com>
2026-02-19 19:50:39 +0300

    feat : add code quality issue

--- WebApplication1/WeatherForecast.cs
+++ WebApplication1/WeatherForecast.cs
@@ -8,7 +8,6 @@ namespace WebApplication1
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
-        bdhjb
+        public string Summary { get; set; }
     }
 }
